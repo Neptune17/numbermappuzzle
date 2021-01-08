a6a6ec0ca6b95bb1a0e62c9b079f5936b7cdcd64
Neptune17 <1115876559@qq.com>
2021-01-08 15:14:48 +0800

    speed improvement

--- main.py
+++ main.py
@@ -1,52 +1,94 @@
 from painter import *
 import time
 
-def check(x, cnt):
-    temp = cnt
-    index = -1
-    for i in x:
-        if temp == 0:
-            return 1
-        if temp < 0:
-            return 2
-        temp -= i
-    if temp < 0:
-        return 2
-    if temp == 0:
-        return 3
-
-def DFS1(mat, x, y, line, loc, available, mark):
-    if len(y[line]) == loc:
-        return DFS(mat, x, y, line + 1, mark)
-    for i in range(available, len(x)):
-        flag = True
-        for j in range(i, i + y[line][loc]):
-            if j >= len(x):
-                flag = False
-                continue
-            if check(x[j], mark[j]) == 1 and (line > 0 and mat[j,line - 1] == 1):
-                flag = False
-            if check(x[j], mark[j]) == 2 and (line == 0 or mat[j,line - 1] == 0):
-                flag = False
-            if check(x[j], mark[j]) == 3:
-                flag = False
-            mat[j, line] = 1
-            mark[j] += 1
-        if flag:
-            ret = DFS1(mat, x, y, line, loc + 1, i + y[line][loc] + 1, mark)
-            if ret:
-                return ret
-        for j in range(i, i + y[line][loc]):
-            if j >= len(x):
-                continue
-            mat[j, line] = 0
-            mark[j] -= 1
-    return False
-
-def DFS(mat, x, y, line, mark):
-    if line == len(y):
+def DFS(Map, YStack, YStack_bound, x, y, layer):
+    if layer == len(x):
         return True
-    return DFS1(mat, x, y, line, 0, 0, mark)
+
+    global_check = True
+    for i in range(len(y)):
+        if YStack[i] != len(y[i]) and YStack_bound[i][YStack[i]] < layer:
+            global_check = False
+            break
+
+    if not global_check:
+        return False
+
+    # print("layer: " + str(layer), x[layer])
+
+    state = [0 for i in range(len(x[layer]))]
+    end_state = [0 for i in range(len(x[layer]))]
+    pre_end = 0
+    pre_start = len(x) - x[layer][-1]
+    for i in range(len(x[layer])):
+        state[i] = pre_end
+        pre_end += x[layer][i] + 1
+    for i in range(len(x[layer]) - 1, -1, -1):
+        end_state[i] = pre_start
+        if i - 1 >= 0:
+            pre_start -= x[layer][i - 1] + 1
+    
+    # print(state)
+    # print(end_state)
+
+    while(True):
+        # convert current state
+        templayer = [0 for i in range(len(x))]
+        
+        for i in range(len(state)):
+            for j in range(state[i], state[i] + x[layer][i]):
+                templayer[j] = 1
+        
+        # print(templayer)
+
+        # check current state
+        local_check = True
+        for i in range(len(x)):
+            if templayer[i] == 0 and Map[layer][i] == -1:
+                local_check = False
+                break
+            if templayer[i] == 1 and Map[layer][i] == 0:
+                if YStack[i] == len(y[i]):
+                    local_check = False
+                    break
+                if layer > 0 and Map[layer - 1][i] == 1:
+                    local_check = False
+                    break
+
+        # if check ok, move to next layer
+        if local_check:
+            save_layer = [Map[layer][i] for i in range(len(x))]
+            for i in range(len(x)):
+                if templayer[i] == 1 and save_layer[i] == 0:
+                    ysize = y[i][YStack[i]]
+                    YStack[i] += 1
+                    for j in range(layer, layer + ysize):
+                        Map[j][i] = -1
+                Map[layer][i] = templayer[i]
+            ret = DFS(Map, YStack, YStack_bound, x, y, layer + 1)
+            if ret:
+                return True
+            for i in range(len(x)):
+                if templayer[i] == 1 and save_layer[i] == 0:
+                    YStack[i] -= 1
+                    ysize = y[i][YStack[i]]
+                    for j in range(layer, layer + ysize):
+                        Map[j][i] = 0
+                Map[layer][i] = save_layer[i]
+
+        # final state, return to pre layer
+        if state == end_state:
+            break
+            
+        # move to next state
+        for i in range(len(state) - 1, -1, -1):
+            if state[i] != end_state[i]:
+                state[i] = state[i] + 1
+                for j in range(i + 1 , len(state)):
+                    state[j] = state[j - 1] + x[layer][j - 1] + 1
+                break
+
+    return False
 
 def generate_label(x, y):
     xlabel = []
@@ -68,43 +110,43 @@ def generate_label(x, y):
     return xlabel, ylabel
 
 if __name__ == '__main__':
-    x = [
-        [4],
-        [6,4],
-        [12,1],
-        [2,1,6],
-        [2,5,1],
-        [3,1,6],
-        [3,7],
-        [3,6],
-        [3,1,5,1],
-        [2,6],
-        [12,1],
-        [6,4],
-        [4],
-        [3],
-        [5]
-    ]
+    # row = [
+    #     [4],
+    #     [6,4],
+    #     [12,1],
+    #     [2,1,6],
+    #     [2,5,1],
+    #     [3,1,6],
+    #     [3,7],
+    #     [3,6],
+    #     [3,1,5,1],
+    #     [2,6],
+    #     [12,1],
+    #     [6,4],
+    #     [4],
+    #     [3],
+    #     [5]
+    # ]
 
-    y = [
-        [7],
-        [9],
-        [2,4,2],
-        [2,2],
-        [2,1,1,2],
-        [3,2],
-        [2,2],
-        [3,3,1],
-        [9,1],
-        [9,1],
-        [11,2],
-        [15],
-        [14],
-        [2,3,2],
-        [1,1,1,1,1,1,1]
-    ]
+    # col = [
+    #     [7],
+    #     [9],
+    #     [2,4,2],
+    #     [2,2],
+    #     [2,1,1,2],
+    #     [3,2],
+    #     [2,2],
+    #     [3,3,1],
+    #     [9,1],
+    #     [9,1],
+    #     [11,2],
+    #     [15],
+    #     [14],
+    #     [2,3,2],
+    #     [1,1,1,1,1,1,1]
+    # ]
 
-    # x = [
+    # row = [
     #     [2],
     #     [4,2],
     #     [1,2,1],
@@ -117,7 +159,7 @@ if __name__ == '__main__':
     #     [2]
     # ]
 
-    # y = [
+    # col = [
     #     [2,3],
     #     [1,5],
     #     [1,2,2],
@@ -129,11 +171,58 @@ if __name__ == '__main__':
     #     [2,2,1,1],
     #     [6,2]
     # ]
+
+    row = [
+        [1,1,1],
+        [2,1,2,2],
+        [1,1,5,1],
+        [1,3,1],
+        [1,1,3,3,1],
+        [3,1],
+        [5,1],
+        [2,1,2,1],
+        [1,1,1],
+        [5,1,2],
+        [2,4,1,2],
+        [1,2,2,2],
+        [1,2,1,2,3],
+        [1,6,1],
+        [13,1]
+    ]
     
-    xlabel, ylabel = generate_label(x, y)
-    mat=np.zeros((len(x),len(y)))
+    col = [
+        [1,1,1,3,1],
+        [1,2,1],
+        [1,1,1,1,4],
+        [1,2,1],
+        [2,1],
+        [1,1,1,2,1,1],
+        [2,1,2,2,1],
+        [5,6],
+        [4,4,3],
+        [5,1,2],
+        [2,1,2,2],
+        [1,1,1,3],
+        [5],
+        [4,4],
+        [2,5,2]
+    ]
+
+    rowlabel, collabel = generate_label(row, col)
+    AnsMap = np.zeros((len(row),len(col)))
+    ColStack = [0 for i in range(len(col))]
+    ColStack_bound = []
+    for i in range(len(col)):
+        ColStack_bound.append([0 for i in range(len(col[i]))])
+        pre_start = len(row) - col[i][-1]
+        for j in range(len(col[i]) - 1, -1, -1):
+            ColStack_bound[i][j] = pre_start
+            if j - 1 >= 0:
+                pre_start -= col[i][j - 1] + 1
+
     begin = time.time()
-    print(DFS(mat, x, y, 0, [0 for i in range(len(x))]))
+    ret = DFS(AnsMap, ColStack, ColStack_bound, row, col, 0)
     end = time.time()
-    print("time:", end - begin)
-    paint_map(mat.T, xlabel, ylabel, "result.jpg")
+    
+    print("time:", end - begin, ret)
+    paint_map(AnsMap.T, rowlabel, collabel, "result.jpg")
